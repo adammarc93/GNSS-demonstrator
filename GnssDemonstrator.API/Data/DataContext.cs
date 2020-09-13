d83bc71c87299d69a481d8ff50e6d89faf40ef2f
adammarc93 <adammarc93@gmail.com>
2020-09-14 00:26:28 +0200

    Added first test model

---
+++ GnssDemonstrator.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using GnssDemonstrator.API.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace GnssDemonstrator.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :  base(options) { }
+
+        public DbSet<Value> Values { get; set; }  
+    }
+}
