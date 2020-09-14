52b05ec88f9b9b283fa7332822f8c1a902301edd
adammarc93 <adammarc93@gmail.com>
2020-09-15 01:58:56 +0200

    Added user login on SPA

--- GnssDemonstrator.SPA/src/app/nav/nav.component.ts
+++ GnssDemonstrator.SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  login(){
+    console.log(this.model);
+
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Zalogowałeś się do aplikacji');
+    }, error => {
+      console.log('Wystąpił błąd logowania');
+    });
+  }
 }
