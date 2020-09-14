a790ff32e143e1063b973fbdc41abb426f0794d5
adammarc93 <adammarc93@gmail.com>
2020-09-14 02:37:53 +0200

    Added User class

--- GnssDemonstrator.API/Migrations/20200914003428_AddedUser.Designer.cs
+++ GnssDemonstrator.API/Migrations/20200914003428_AddedUser.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GnssDemonstrator.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GnssDemonstrator.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200914003428_AddedUser")]
+    partial class AddedUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
