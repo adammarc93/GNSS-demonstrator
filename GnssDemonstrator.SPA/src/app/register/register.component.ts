025037b704836e3bbd6c9ed274844bec57080cca
adammarc93 <adammarc93@gmail.com>
2020-09-16 01:23:32 +0200

    Added user register method on SPA

--- GnssDemonstrator.SPA/src/app/register/register.component.ts
+++ GnssDemonstrator.SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,21 +8,21 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  // Data from parent
-  @Input() valuesFromHome: any;
-
-  // Data to parent
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('rejestracja udana');
+    }, error => {
+      console.log(error);
+    });
   }
 
   cancel() {
