b66b44f88afd67542e886c81246b339a1e4a18b7
adammarc93 <adammarc93@gmail.com>
2020-09-17 23:22:57 +0200

    Added users list component on SPA

--- GnssDemonstrator.SPA/src/app/app.module.ts
+++ GnssDemonstrator.SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -10,24 +11,33 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
 import { UserService } from './_services/user.service';
+import { UsersListComponent } from './users/users-list/users-list.component';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
-   ],
+    RegisterComponent,
+    UsersListComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        allowedDomains: ['localhost:5000'],
+        disallowedRoutes: ['localhost:5000/api/auth']
+      }
+    })
   ],
-  providers: [
-    AuthService,
-    AlertifyService,
-    UserService
-  ],
+  providers: [AuthService, AlertifyService, UserService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
